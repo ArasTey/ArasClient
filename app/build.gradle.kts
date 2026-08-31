70dcb918cc89b9be3ac51d47634dfb7c3c2a866b
ArasTey <ArasTey@users.noreply.github.com>
2026-08-31 21:27:22 +0330

    versionCode 110

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -20,7 +20,7 @@ android {
         applicationId = "com.aras.client"
         minSdk = 24
         targetSdk = 37
-        versionCode = 109
+        versionCode = 110
         versionName = "1.3.5-beta"
 
         val abiFilterList = (properties["ABI_FILTERS"] as? String)?.split(';')
